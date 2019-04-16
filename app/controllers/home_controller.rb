3f52fb29e214a79ff5360f36991175e93d7b6890
avoneiff <avoneiff@tamu.edu>
2019-04-16 19:14:43 +0000

    UI is fleshed out and ready for CSS and conrtol bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,42 @@
 class HomeController < ApplicationController
   def index
+    # for csv.count()
+    # get start time
+    # get end time
+    # apply to series
+    
+    @HR_series_a =  [[2, 100], [16, 100]]
+    @HR_series_b =  [[2, 60], [16, 60]]
+    
+    @SBP_series_a =  [[2, 110], [16, 110]]
+    @SBP_series_b =  [[2, 130], [16, 130]]
+    @SBP_series_c =  [[2, 180], [16, 180]]
+    
+    
+    @DBP_series_a =  [[2, 70], [16, 70]]
+    @DBP_series_b =  [[2, 80], [16, 80]]
+    @DBP_series_c =  [[2, 120], [16, 120]]
+    
+    @Temp_series_a =  [[2, 100], [16, 100]]
+    @Temp_series_b =  [[2, 95], [16, 95]]
+    
+    @SP_series_a =  [[2, 90], [16, 90]]
+    
+    @patient_risk = 52
+    @model1_risk = 0;
+    @model2_risk = 0;
+    @model2_risk = 0;
+    @model2_risk = 0;
+    
+    @mdoel_variable1 = 'Heart Rate'
+    @mdoel_variable2 = 'SpO2'
+    
+    @image_patient_risk = 50
+    @image_path = '/assets/risk_' + @image_patient_risk.to_s + '.jpg'
+    
+    @doctor_notes = "These are the doctors notes."
+    @nurse_notes = "These are the nurse notes."
+    @discharge_notes = "These are the discharge notes."
+    
   end
 end
