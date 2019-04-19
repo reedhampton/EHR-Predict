9c4a2575b5f09ca2c80d92ad64b08e92be369bce
Reed Hampton <reedhampton@tamu.edu>
2019-04-19 17:14:01 +0000

    Cleaned up the Python Script

--- lib/assets/clean_gcs_total_data.py
+++ lib/assets/clean_gcs_total_data.py
@@ -11,37 +11,35 @@ with open(filename, 'rb') as csvFile, tempfile:
     
     #GCS_Total is the currently 16th column
     rows = []
-    skip_head_row = True
-    copied_gcs_value = None
-    skip_rows = 0
+    on_head_row = True
+    gcs_value = None
+    gcs_total_pos = 15
     
-    #Determine the value we need to fill the beginning data cells with
+    #Determine the value we need to fill the beginning data cells with if they are null
     for row in reader:
-        skip_rows = skip_rows + 1
-        if not skip_head_row:
-            if row[15] == '':
+        if not on_head_row:
+            if row[gcs_total_pos] == '':
                 rows.append(row)
                 continue
             else:
-                copied_gcs_value = row[15]
+                gcs_value = row[gcs_total_pos]
                 rows.append(row)
                 break
-        skip_head_row = False
+        on_head_row = False
         writer.writerow(row)
     
     #Write the first rows that had a null entry
     for row in rows:
-        row[15] = copied_gcs_value
+        row[gcs_total_pos] = gcs_value
         writer.writerow(row)
             
-    #Keep reading and writing
+    #Keep reading and writing and updating each row as needed
     for row in reader:
-        if row[15] == '':
-            row[15] = copied_gcs_value
+        if row[gcs_total_pos] == '':
+            row[gcs_total_pos] = gcs_value
         else:
-            copied_gcs_value = row[15]
+            gcs_value = row[gcs_total_pos]
         writer.writerow(row)
 
-
-#Write to our updated file
+#Write to our updated file CHANGE TO 'filename' WHEN OTHER SCRIPTS ARE DONE
 shutil.move(tempfile.name, '../../app/assets/data/sample_data.csv')
