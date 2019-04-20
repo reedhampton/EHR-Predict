4967ac69a761af78c4279e7c582fbd446c1313f8
avoneiff <avoneiff@tamu.edu>
2019-04-20 20:43:08 +0000

    updated the graphs data, still need to get session variables working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,6 @@
+require 'csv'
+
+
 class HomeController < ApplicationController
   def index
     flash.clear
@@ -11,23 +14,35 @@ class HomeController < ApplicationController
     # get start time
     # get end time
     # apply to series
+    table = CSV.read("#{Rails.root}/uploads/Patient Data.csv", headers: true)
+    @data_start_time = table['charttime'].values_at(0)
+    @data_end_time = table['charttime'].values_at(-1)
+    time = table['charttime']
+    hr = table['heart_rate']
+    bp = table['abp_systolic']
+    gs = table['gcs_total']
+    pl = table['platelets']
     
-    @HR_series_a =  [[2, 100], [16, 100]]
-    @HR_series_b =  [[2, 60], [16, 60]]
+    @HR_series_data = [time, hr].transpose
+    @HR_series_a =  [[@data_start_time, 100], [@data_end_time, 100]]
+    @HR_series_b =  [[@data_start_time, 60], [@data_end_time, 60]]
     
-    @SBP_series_a =  [[2, 110], [16, 110]]
-    @SBP_series_b =  [[2, 130], [16, 130]]
-    @SBP_series_c =  [[2, 180], [16, 180]]
+    @SBP_series_data = [time, bp].transpose
+    @SBP_series_a =  [[@data_start_time, 110], [@data_end_time, 110]]
+    @SBP_series_b =  [[@data_start_time, 130], [@data_end_time, 130]]
+    @SBP_series_c =  [[@data_start_time, 180], [@data_end_time, 180]]
+   
+    @DBP_series_data = [time, bp].transpose
+    @DBP_series_a =  [[@data_start_time, 70], [@data_end_time, 70]]
+    @DBP_series_b =  [[@data_start_time, 80], [@data_end_time, 80]]
+    @DBP_series_c =  [[@data_start_time, 120], [@data_end_time, 120]]
     
+    @Temp_series_data = [time, gs].transpose
+    @Temp_series_a =  [[@data_start_time, 100], [@data_end_time, 100]]
+    @Temp_series_b =  [[@data_start_time, 95], [@data_end_time, 95]]
     
-    @DBP_series_a =  [[2, 70], [16, 70]]
-    @DBP_series_b =  [[2, 80], [16, 80]]
-    @DBP_series_c =  [[2, 120], [16, 120]]
-    
-    @Temp_series_a =  [[2, 100], [16, 100]]
-    @Temp_series_b =  [[2, 95], [16, 95]]
-    
-    @SP_series_a =  [[2, 90], [16, 90]]
+    @SP_series_data = [time, pl].transpose
+    @SP_series_a =  [[@data_start_time, 90], [@data_end_time, 90]]
     
     @patient_risk = 90
     @model1_risk = 0
@@ -41,12 +56,13 @@ class HomeController < ApplicationController
     @image_patient_risk = round_to_next_5(@patient_risk)
     @image_path = '/assets/risk_' + @image_patient_risk.to_s + '.jpg'
     
-    @doctor_notes = "These are the doctors notes."
+    @doctor_notes = "Doctor text"
     @event_notes = "These are the event notes."
     @discharge_notes = "These are the discharge notes."
   end
   
   def create
+    session[:id] = 0
     flash.clear
     #Uploads the CSV into the /uploads directory
     uploaded_io = params[:user_input][:data]
@@ -65,6 +81,8 @@ class HomeController < ApplicationController
       #Get the returns from the Fancy python Script
       #Parse those returns
       #Put them in sessions
+      assign_session_data() #does nothing right now
+      
       #Delete teh CSV
       
       redirect_to '/results'
@@ -92,6 +110,12 @@ class HomeController < ApplicationController
       filename: "sample_data.csv"
     )
   end
+  
+  def assign_session_data()
+    
+  end
 end
+
+
 
 
